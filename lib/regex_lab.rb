155aec21e05d2c687f7990466e2f03564f01128d
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-27 03:34:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,11 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiou]\w*/) != nil
-    return bool = true
-  end
-  if word.match(/\b[aeiou]\w*/) == nil
-    bool = false
-  end
-return word.match(/\b[aeiou]\w*/)
+  word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
